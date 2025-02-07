96a4b0e4ee1eb85850cc7afc50310f9c7748d8ff
Safi222 <safia2000gibril@gmail.com>
2025-02-07 14:43:10 -0800

    files

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,3 @@
-/**
- * * Creates a function named displayMessage that prints in STDOUT the string argument.
- * * @param {string} message - The message to be displayed.
- * */
-function displayMessage(message) {
-	console.log(message);
-}
-module.exports = displayMessage;
+module.exports = function displayMessage(str) {
+  console.log(str);
+};
